b6da69acdc0ba07f994c0c4977ffcc711fed22b4
jacobbieker <jbieker@uoregon.edu>
2017-10-14 23:34:09 -0700

    Add envs file to gitignore

--- data_io.py
+++ data_io.py
@@ -1,2 +1,2 @@
-import os
 import numpy as np
+import os
--- get_fact_data.py
+++ get_fact_data.py
@@ -1,1 +1,12 @@
 import os, sys
+import requests
+import yaml
+
+with open("envs.yaml", "r") as credential_file:
+    credentials = yaml.load(credential_file)
+
+frontpage = requests.get(credentials['fact']['url'], auth=(credentials['fact']['username'], credentials['fact']['password']))
+print(frontpage.text)
+
+
+
