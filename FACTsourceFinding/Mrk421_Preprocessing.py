74930da184d9466dfbf908836ca25a38320b5b06
Jacob Bieker <jacob@bieker.tech>
2018-04-27 21:27:23 -0700

    Update

--- FACTsourceFinding/Mrk421_Preprocessing.py
+++ FACTsourceFinding/Mrk421_Preprocessing.py
@@ -18,7 +18,7 @@ from fact.io import read_h5py
 path_raw_crab_folder = "/run/media/jacob/WDRed8Tb2/ihp-pc41.ethz.ch/public/phs/obs/"
 #path_store_mapping_dict = sys.argv[2]
 path_runs_to_use = "/run/media/jacob/SSD/Development/thesis/FACTsourceFinding/runs/Mrk 421.csv"
-path_store_mapping_dict = "/run/media/jacob/SSD/Development/thesis/jan/07_make_FACT/rebinned_mapping_dict.p"
+path_store_mapping_dict = "/run/media/jacob/SSD/Development/thesis/jan/07_make_FACT/rebinned_mapping_dict_4_flipped.p"
 #path_mc_images = sys.argv[3]
 path_crab_images = "/run/media/jacob/WDRed8Tb1/Rebinned_2_mrk421_std_analysis_preprocessed_images.h5"
 path_std_analysis = "/run/media/jacob/WDRed8Tb1/dl2_theta/precuts/std_analysis/Mrk421_1516_std_analysis_0.17.2.hdf5"
@@ -26,7 +26,7 @@ path_store_runlist = "Mrk421_std_analysis.p"
 
 # Format dataset to fit into tensorflow
 def reformat(dataset):
-    return dataset.reshape((-1, 186, 186, 1)).astype(np.float32)
+    return dataset.reshape((-1, 75, 75, 1)).astype(np.float32)
 
 
 def batchYielder():
@@ -73,7 +73,7 @@ def batchYielder():
                     trigger = line_data['Trigger']
                     time = line_data['UnixTime_s_us'][0] + 1e-6*line_data['UnixTime_s_us'][1]
 
-                    input_matrix = np.zeros([186,186])
+                    input_matrix = np.zeros([75,75])
                     chid_to_pixel = id_position[0]
                     pixel_index_to_grid = id_position[1]
                     for index in range(1440):
--- jan/07_make_FACT/python_files/01a_Preprocess_Crab_Images.py
+++ jan/07_make_FACT/python_files/01a_Preprocess_Crab_Images.py
@@ -15,16 +15,16 @@ import sys
 #path_store_mapping_dict = sys.argv[3]
 #path_crab_images = sys.argv[4]
 
-path_raw_crab_folder = "/projects/sventeklab/jbieker/raw_data/ihp-pc41.ethz.ch/public/phs/obs/"
+path_raw_crab_folder = "/run/media/jacob/WDRed8Tb2/ihp-pc41.ethz.ch/public/phs/obs/"
 #path_store_mapping_dict = sys.argv[2]
-path_runs_to_use = "/projects/sventeklab/jbieker/git-thesis/jan/07_make_FACT/Crab1314_runs_to_use.csv"
-path_store_mapping_dict = "/projects/sventeklab/jbieker/git-thesis/jan/07_make_FACT/rebinned_mapping_dict_flipped.p"
+path_runs_to_use = "/run/media/jacob/SSD/Development/thesis/jan/07_make_FACT/Crab1314_runs_to_use.csv"
+path_store_mapping_dict = "/run/media/jacob/SSD/Development/thesis/jan/07_make_FACT/rebinned_mapping_dict_4_flipped.p"
 #path_mc_images = sys.argv[3]
-path_crab_images = "/projects/sventeklab/jbieker/FACTSources/Rebinned_2_flipped_crab1314_preprocessed_images.h5"
+path_crab_images = "/run/media/jacob/HDD/Rebinned_5_flipped_crab1314_preprocessed_images.h5"
 
 # Format dataset to fit into tensorflow
 def reformat(dataset):
-    return dataset.reshape((-1, 186, 186, 1)).astype(np.float32)
+    return dataset.reshape((-1, 75, 75, 1)).astype(np.float32)
 
 
 def batchYielder():
@@ -60,7 +60,7 @@ def batchYielder():
                     trigger = line_data['Trigger']
                     time = line_data['UnixTime_s_us'][0] + 1e-6*line_data['UnixTime_s_us'][1]
 
-                    input_matrix = np.zeros([186,186])
+                    input_matrix = np.zeros([75,75])
                     chid_to_pixel = id_position[0]
                     pixel_index_to_grid = id_position[1]
                     for index in range(1440):
