a53f4aa8a2f6c504dae146d0cc1969617a2a29f1
Jacob Bieker <jacob@bieker.tech>
2018-05-01 11:39:43 -0700

    Update to fix issues with 0 sized batches

--- FACTsourceFinding/mc_sep_nn.py
+++ FACTsourceFinding/mc_sep_nn.py
@@ -20,13 +20,13 @@ else:
 
 # Hyperparameters
 
-number_of_training = 1000000 * (0.6)
-number_of_testing = 1000000 * (0.2)
-number_validate = 1000000 * (0.2)
+number_of_training = 100000 * (0.6)
+number_of_testing = 100000 * (0.2)
+number_validate = 100000 * (0.2)
 num_labels = 2
 
 # Total fraction to use per epoch of training data, need inverse
-frac_per_epoch = 5
+frac_per_epoch = 1
 num_epochs = 100*frac_per_epoch
 
 path_mc_images = base_dir + "/FACTSources/Rebinned_5_MC_Phi_Images.h5"
@@ -44,6 +44,9 @@ def metaYielder():
 
     gamma_count = int(round(number_of_training * gamma_anteil))
     hadron_count = int(round(number_of_training * hadron_anteil))
+
+    gamma_anteil = 0.5
+    hadron_anteil = 0.5
 
     return gamma_anteil, hadron_anteil, gamma_count, hadron_count
 
@@ -71,16 +74,15 @@ def create_model(batch_size, patch_size, dropout_layer, num_dense, num_conv, num
                      "_denseN_" + str(dense_neuron) + "_convN_" + str(conv_neurons) + ".h5"
         if not os.path.isfile(model_name):
             model_checkpoint = keras.callbacks.ModelCheckpoint(model_name,
-                                                               monitor='val_loss',
+                                                               monitor='val_acc',
                                                                verbose=0,
                                                                save_best_only=True,
                                                                save_weights_only=False,
                                                                mode='auto', period=1)
-            early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0,
-                                                       patience=3 * frac_per_epoch,
+            early_stop = keras.callbacks.EarlyStopping(monitor='val_acc', min_delta=0,
+                                                       patience=10 * frac_per_epoch,
                                                        verbose=0, mode='auto')
 
-            gamma_anteil, hadron_anteil, gamma_count, hadron_count = metaYielder()
             def batchYielder():
                 gamma_anteil, hadron_anteil, gamma_count, hadron_count = metaYielder()
                 with h5py.File(path_mc_images, 'r') as f:
@@ -91,6 +93,8 @@ def create_model(batch_size, patch_size, dropout_layer, num_dense, num_conv, num
                     image = f['GammaImage'][0:int(np.floor((gamma_anteil * number_of_training)))]
                     #np.random.set_state(rng_state)
                     image_false = f['Image'][0:int(np.floor((hadron_anteil * number_of_training)))]
+                    if items > len(image_false):
+                        items = len(image_false)
                     while True:
                         # Get some truth data for now, just use Crab images
                         batch_num = 0
@@ -127,14 +131,16 @@ def create_model(batch_size, patch_size, dropout_layer, num_dense, num_conv, num
                            padding='same'))
                 if num_pooling_layer == 1:
                     model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-                model.add(Dropout(dropout_layer))
+                if dropout_layer > 0.0:
+                    model.add(Dropout(dropout_layer))
 
             model.add(Flatten())
 
             # Now do the dense layers
             for i in range(num_dense):
                 model.add(Dense(dense_neuron, activation='relu'))
-                model.add(Dropout(dropout_layer))
+                if dropout_layer > 0.0:
+                    model.add(Dropout(dropout_layer))
 
             # Final Dense layer
             model.add(Dense(num_labels, activation='softmax'))
@@ -159,19 +165,19 @@ def create_model(batch_size, patch_size, dropout_layer, num_dense, num_conv, num
 batch_sizes = [16,256]
 patch_sizes = [(2, 2), (3, 3), (4, 4), (5, 5)]
 dropout_layers = [0.0, 1.0]
-num_conv_layers = [0, 8]
-num_dense_layers = [0, 8]
-num_conv_neurons = [8,256]
-num_dense_neuron = [8,1024]
-num_pooling_layers = [0, 1, 2]
-num_runs = 100
+num_conv_layers = [0, 6]
+num_dense_layers = [0, 6]
+num_conv_neurons = [8,128]
+num_dense_neuron = [8,256]
+num_pooling_layers = [0, 2]
+num_runs = 500
 
 if architecture == "intel":
     for i in range(num_runs):
         # On Talapas so can use the 4 gpus
         with tf.device('/gpu:0'):
             # One copy
-            dropout_layer = np.random.uniform(0.0, 1.0, size=1)
+            dropout_layer = 0.0 #np.random.uniform(0.0, 1.0, size=1)
             batch_size = np.random.randint(batch_sizes[0], batch_sizes[1])
             num_conv = np.random.randint(num_conv_layers[0], num_conv_layers[1])
             num_dense = np.random.randint(num_dense_layers[0], num_dense_layers[1])
@@ -219,11 +225,11 @@ if architecture == "intel":
 
 else:
     for i in range(num_runs):
-        dropout_layer = np.random.uniform(0.0, 1.0, size=1)
+        dropout_layer = 0.0 #np.round(np.random.uniform(0.0, 1.0, size=1), 2)
         batch_size = np.random.randint(batch_sizes[0], batch_sizes[1])
         num_conv = np.random.randint(num_conv_layers[0], num_conv_layers[1])
         num_dense = np.random.randint(num_dense_layers[0], num_dense_layers[1])
-        patch_size = patch_sizes[np.random.randint(0,4)]
+        patch_size = patch_sizes[1]
         num_pooling_layer = np.random.randint(num_pooling_layers[0], num_pooling_layers[1])
         dense_neuron = np.random.randint(num_dense_neuron[0], num_dense_neuron[1])
         conv_neurons = np.random.randint(num_conv_neurons[0], num_conv_neurons[1])
