Fix errors with flow add normalization
Now can normalize per iamge basis for the single_processor.

Also, flow_from_files_separation is now for some reason working very well, vs not at all before at predicting. I think something is wrong....

Other misc fixes and testing done.

factnn/data/augment.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.utils import shuffle
 import h5py
+from keras.utils import to_categorical
 
 
 def image_augmenter(images):
@@ -251,6 +252,14 @@ def true_delta(cog_y, source_y, cog_x, source_x):
         cog_x - source_x
     )
 
+def true_sign(source_x, source_y, cog_x, cog_y, delta):
+
+    true_delta = np.arctan2(
+        cog_y - source_y,
+        cog_x - source_x,
+        )
+    true_sign = np.sign(np.abs(delta - true_delta) - np.pi / 2)
+    return true_sign
 
 def get_random_from_paths(preprocessor, size, time_slice, total_slices,
                           proton_preprocessor=None, type_training=None, augment=True, swap=True, shape=None):
@@ -298,9 +307,17 @@ def get_random_from_paths(preprocessor, size, time_slice, total_slices,
         labels = np.array(labels)
         training_data = [item[data_format["Image"]] for item in training_data]
     elif type_training == "Sign":
-        labels = [true_delta(item[data_format['Source_X']], item[data_format['Source_Y']],
-                             item[data_format['COG_X']], item[data_format['COG_Y']]) for item in training_data]
+        labels = [true_sign(item[data_format['Source_X']], item[data_format['Source_Y']],
+                             item[data_format['COG_X']], item[data_format['COG_Y']], item[data_format['Delta']]) for item in training_data]
         labels = np.array(labels)
+        # Create own categorical one since only two sides anyway
+        new_labels = np.zeros((labels.shape[0],2))
+        for index, element in enumerate(labels):
+            if element < 0:
+                new_labels[index][0] = 1.
+            else:
+                new_labels[index][1] = 1.
+        labels = new_labels
         training_data = [item[data_format["Image"]] for item in training_data]
 
     training_data = np.array(training_data)

factnn/preprocess/simulation_preprocessors.py
@@ -41,7 +41,7 @@ class ProtonPreprocessor(BasePreprocessor):
             except Exception as e:
                 print(str(e))
 
-    def single_processor(self):
+    def single_processor(self, normalize=False):
         while True:
             self.paths = shuffle(self.paths)
             for index, file in enumerate(self.paths):
@@ -72,7 +72,12 @@ class ProtonPreprocessor(BasePreprocessor):
                                         input_matrix[coords[0]][coords[1]][value - self.start] += element[1] * 1
                         data.append([np.fliplr(np.rot90(input_matrix, 3)), energy, zd_deg, az_deg, act_phi, act_theta])
                         data_format = {'Image': 0, 'Energy': 1, 'Zd_Deg': 2, 'Az_Deg': 3, 'COG_Y': 4, 'Phi': 5, 'Theta': 6,}
-                        yield self.format(data), data_format
+                        data = self.format(data)
+                        if normalize:
+                            data = list(data)
+                            data[0] = self.normalize_image(data[0])
+                            data = tuple(data)
+                        yield data, data_format
 
                 except Exception as e:
                     print(str(e))
@@ -198,7 +203,7 @@ class GammaPreprocessor(BasePreprocessor):
             except Exception as e:
                 print(str(e))
 
-    def single_processor(self):
+    def single_processor(self, normalize=False):
         while True:
             self.paths = shuffle(self.paths)
             for index, file in enumerate(self.paths):
@@ -226,10 +231,15 @@ class GammaPreprocessor(BasePreprocessor):
                                 coords = pixel_index_to_grid[element[0]]
                                 for value in event_photons[index]:
                                     if self.end > value > self.start:
-                                        input_matrix[coords[0]][coords[1]][value - self.start] += element[1] * 1
+                                        input_matrix[coords[0]][coords[1]][value - self.start] += element[1] * 100
                         data.append([np.fliplr(np.rot90(input_matrix, 3)), energy, zd_deg, az_deg, act_phi, act_theta])
                         data_format = {'Image': 0, 'Energy': 1, 'Zd_Deg': 2, 'Az_Deg': 3, 'COG_Y': 4, 'Phi': 5, 'Theta': 6,}
-                        yield self.format(data), data_format
+                        data = self.format(data)
+                        if normalize:
+                            data = list(data)
+                            data[0] = self.normalize_image(data[0])
+                            data = tuple(data)
+                        yield data, data_format
 
                 except Exception as e:
                     print(str(e))
@@ -369,7 +379,7 @@ class GammaDiffusePreprocessor(BasePreprocessor):
             except Exception as e:
                 print(str(e))
 
-    def single_processor(self):
+    def single_processor(self, normalize=False):
         while True:
             self.paths = shuffle(self.paths)
             for index, file in enumerate(self.paths):
@@ -410,15 +420,20 @@ class GammaDiffusePreprocessor(BasePreprocessor):
                                     for value in event_photons[index]:
                                         if self.end > value > self.start:
                                             input_matrix[coords[0]][coords[1]][value - self.start] += element[1] * 1
-
                             data.append([np.fliplr(np.rot90(input_matrix, 3)), act_sky_source_zero, act_sky_source_one,
                                          cog_x, cog_y, zd_deg, az_deg, sky_source_zd, sky_source_az, delta,
                                          energy, zd_deg1, az_deg1])
                         # Add an associated structure that gives the name?
                         data_format = {'Image': 0, 'Source_X': 1, 'Source_Y': 2, 'COG_X': 3, 'COG_Y': 4, 'Zd_Deg': 5, 'Az_Deg': 6,
                                        'Source_Zd': 7, 'Source_Az': 8, 'Delta': 9, 'Energy': 10, 'Pointing_Zd': 11, 'Pointing_Az': 12}
+
                         if len(data) != 0:
-                            yield self.format(data), data_format
+                            data = self.format(data)
+                            if normalize:
+                                data = list(data)
+                                data[0] = self.normalize_image(data[0])
+                                data = tuple(data)
+                            yield data, data_format
 
                 except Exception as e:
                     print(str(e))

factnn/utils/cross_validate.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.utils import shuffle
 
+
 def split_data(indicies, kfolds, seeds=None):
     """
     Splits the data into the indicies for kfold validation
@@ -16,7 +17,7 @@ def split_data(indicies, kfolds, seeds=None):
     if seeds is None:
         seeds = []
         for fold in range(kfolds):
-            seeds.append(np.random.randint(0,2**32-1))
+            seeds.append(np.random.randint(0, 2 ** 32 - 1))
 
     # Now split into kfolds,
     list_of_training = []
@@ -30,10 +31,31 @@ def split_data(indicies, kfolds, seeds=None):
 
     for train_index, test_index in kf.split(indicies):
         # Need to split train_index into validation data
-        train_data, validate_data = train_test_split(indicies[train_index], train_size=(1.0-validate_fraction), test_size=validate_fraction)
+        train_data, validate_data = train_test_split(indicies[train_index], train_size=(1.0 - validate_fraction),
+                                                     test_size=validate_fraction)
         list_of_training.append(train_data)
         list_of_validate.append(validate_data)
         list_of_testing.append(indicies[test_index])
 
     # Now convert to a numpy array
     return list_of_training, list_of_validate, list_of_testing
+
+
+def cross_validate(model, data, generator, proton_data=None, proton_generator=None, kfolds=5, preprocessor=None, proton_preprocessor=None,
+                   plot=False):
+    """
+    Performs a k-fold cross validation on a given model and data
+    :param proton_preprocessor: Preprocessor for the proton data, if used, default is None
+    :param preprocessor: Preprocessor for gamma data, if used, usually for streaming files, default None
+    :param model: Keras Model instance to train
+    :param generator: Generator for Keras fit_generator
+    :param proton_generator: Generator for Keras fit_generator if using proton data, default None
+    :param kfolds: Number of folds to do for the k-fold validation
+    :param plot: Whether to plot the output
+    :return:
+    """
+
+
+
+
+    return NotImplementedError