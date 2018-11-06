bf928a6e8d9b09aa23efa4d6f1b93c49b94bb47f
Jacob Bieker <jacob@bieker.tech>
2018-11-06 18:36:55 +0100

    Move creating HDF5 to own file

---
+++ factnn/data/dataset/hdf5.py
@@ -0,0 +1,40 @@
+import h5py
+from factnn import ProtonPreprocessor, GammaPreprocessor, GammaDiffusePreprocessor
+
+def create_dataset(paths, preprocessor, output_file="proton.hdf5"):
+    """
+    Create an HDF5 dataset from a preprocessor's output
+    :param paths: Paths to files to send to the preprocessor
+    :param preprocessor: The preprocessor to use, outputs data, data_format on each call of next
+    :param output_file: Name of the output file
+    :return:
+    """
+    gen = preprocessor.batch_processor()
+    batch, data_format = next(gen)
+    formatted_batch = preprocessor.format(batch)
+    row_count = formatted_batch[data_format["Image"]].shape[0]
+    print(row_count)
+
+    with h5py.File(output_file, 'w') as hdf:
+        # Now go through each key in data_format, create the dataset
+        dset_sets = [None for i in formatted_batch]
+        for key, value in data_format:
+            maxshape = (None,) + formatted_batch[value].shape[1:]
+            dset = hdf.create_dataset(key,
+                                      shape=formatted_batch[key].shape,
+                                      maxshape=maxshape,
+                                      chunks=formatted_batch[key].shape,
+                                      dtype=formatted_batch[key].dtype)
+            dset_sets[value] = dset
+            dset_sets[value][:] = formatted_batch[key]
+
+        for batch in gen:
+            batch, data_format = batch
+            formatted_batch = preprocessor.format(batch)
+
+            shape = formatted_batch[1].shape[0]
+            for index, dset in enumerate(dset_sets):
+                dset.resize(row_count + shape, axis=0)
+                dset[row_count:] = formatted_batch[index]
+
+            row_count += shape
