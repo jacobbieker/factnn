275c65d8148991d2e50967036987ddbab8e14d22
Jacob Bieker <jacob@bieker.tech>
2020-07-05 03:24:43 +0200

    Changed name to get rid of shadowing

--- factnn/generator/pytorch/eventfile_dataloader.py
+++ factnn/generator/pytorch/eventfile_dataloader.py
@@ -20,6 +20,10 @@ class EventFileDataset(Dataset):
         self.processed_filenames = []
 
     @property
+    def raw_file_names(self):
+        return []
+
+    @property
     def processed_file_names(self):
         return self.processed_filenames
 
@@ -33,9 +37,9 @@ class EventFileDataset(Dataset):
             if osp.getsize(raw_path) > 0:
                 # Checks that file is not 0
                 with open(raw_path, "rb") as pickled_event:
-                    data, data_format, features, feature_cluster = pickle.load(pickled_event)
+                    event_data, data_format, features, feature_cluster = pickle.load(pickled_event)
                     # Convert List of List to Point Cloud, then truncation is simply cutting in the z direction
-                    event_photons = data[data_format["Image"]]
+                    event_photons = event_data[data_format["Image"]]
                     event_photons = list_of_lists_to_raw_phs(event_photons)
                     point_cloud = np.asarray(raw_phs_to_point_cloud(event_photons,
                                                                     cx=GEOMETRY.x_angle,
@@ -46,13 +50,13 @@ class EventFileDataset(Dataset):
                         data.event_type = torch.tensor(0, dtype=torch.int8)
                     elif "proton" in raw_path:
                         data.event_type = torch.tensor(1, dtype=torch.int8)
-                    data.energy = torch.tensor(data[data_format["Energy"]], dtype=torch.float)
-                    data.disp = torch.tensor(euclidean_distance(data[data_format['Source_X']], data[data_format['Source_Y']],
-                                                                data[data_format['COG_X']], data[data_format['COG_Y']]),
+                    data.energy = torch.tensor(event_data[data_format["Energy"]], dtype=torch.float)
+                    data.disp = torch.tensor(euclidean_distance(event_data[data_format['Source_X']], event_data[data_format['Source_Y']],
+                                                                event_data[data_format['COG_X']], event_data[data_format['COG_Y']]),
                                              dtype=torch.float16)
-                    data.sign = torch.tensor(true_sign(data[data_format['Source_X']], data[data_format['Source_Y']],
-                                                       data[data_format['COG_X']], data[data_format['COG_Y']],
-                                                       data[data_format['Delta']]), dtype=torch.uint8)
+                    data.sign = torch.tensor(true_sign(event_data[data_format['Source_X']], event_data[data_format['Source_Y']],
+                                                       event_data[data_format['COG_X']], event_data[data_format['COG_Y']],
+                                                       event_data[data_format['Delta']]), dtype=torch.uint8)
 
                     if self.pre_filter is not None and not self.pre_filter(data):
                         continue
@@ -70,3 +74,4 @@ class EventFileDataset(Dataset):
     def get(self, idx):
         data = torch.load(osp.join(self.processed_dir, 'data_{}.pt'.format(idx)))
         return data
+
